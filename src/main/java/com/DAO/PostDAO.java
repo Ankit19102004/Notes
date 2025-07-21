72287c0ae0749aa8184e07653bdfba00e1cf87ee
Hp <Hp@LAPTOP-478HASF5>
2025-07-21 10:07:39 +0530

    update in show notes

--- src/main/java/com/DAO/PostDAO.java
+++ src/main/java/com/DAO/PostDAO.java
@@ -1,36 +1,71 @@
 package com.DAO;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.User.Post;
 
 public class PostDAO {
+
 	private Connection conn;
 
 	public PostDAO(Connection conn) {
 		super();
-		this.conn=conn;
-		
+		// TODO Auto-generated constructor stub
+		this.conn = conn;
 	}
-	public boolean AddNotes(String ti,String co,int ui) {
-		boolean f=false;
+
+	public boolean AddNotes(String ti, String co, int ui) {
+		boolean f = false;
 		try {
-			String query="insert into post(title,content,uid) values(?,?,?)";
-			PreparedStatement ps=conn.prepareStatement(query);
-			ps.setString(1, ti);
-			ps.setString(2, co);
-			ps.setInt(3, ui);
-			int i=ps.executeUpdate();
-			if(i==1) {
-				f=true;
+			String query = "insert into post(title,content,uid) values(?,?,?)";
+			PreparedStatement pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, ti);
+			pstmt.setString(2, co);
+			pstmt.setInt(3, ui);
+
+			int i = pstmt.executeUpdate();
+			if (i == 1) {
+				f = true;
 			}
-			
-			
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return f;
 	}
-	
+
+	// To get all posts from post Table
+	public List<Post> getData(int id) {
+
+		List<Post> list = new ArrayList<Post>();
+		Post po;
+		try {
+			String query = "Select * from post where uid=?";
+			PreparedStatement pstmt = conn.prepareStatement(query);
+
+			pstmt.setInt(1, id);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				po = new Post();
+				po.setId(rs.getInt(1));
+				po.setTitle(rs.getString(2));
+				po.setContent(rs.getString(3));
+				po.setPdate(rs.getTimestamp(4));
+
+				list.add(po);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 }
--- src/main/webapp/showNotes.jsp
+++ src/main/webapp/showNotes.jsp
@@ -1,22 +1,61 @@
-<%
-	UserDetails user1=(UserDetails)session.getAttribute("userD");
- 	if(user1==null){
- 		response.sendRedirect("login.jsp");
- 		session.setAttribute("login-error","please lofin first");
- 	}
+<%@page import="com.DAO.PostDAO"%>
+<%@page import="java.util.List"%>
+<%@page import="com.Db.DBConnect"%>
+<%@page import="com.User.Post"%>
 
+<%
+UserDetails user1 = (UserDetails) session.getAttribute("userD");
+if (user1 == null) {
+	response.sendRedirect("login.jsp");
+	session.setAttribute("login-error", "Please Login First!!..");
+}
 %>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
 <%@ include file="all_components/allcss.jsp"%>
-</head>
-<body>
 <%@ include file="all_components/navbar.jsp"%>
+
+<div class="container-fluid ">
+	<h1 class="text-center">Show Notes Page</h1>
+
+	<%
+	if (user1 != null) {
+		PostDAO ob = new PostDAO(DBConnect.getconn());
+		List<Post> post = ob.getData(user1.getId());
+		int count = 0;
+		for (Post po : post) {
+			if (count % 3 == 0) {
+	%>
+	<h1 class="text-center">ShNotes Page</h1>
+
+	<div class="row">
+		<%
+		}
+		%>
+		<div class="col-md-4 mb-5">
+			<div class="card mt-3 w-100 h-100">
+				<div class="card-body p-4">
+					<h4 class="card-title">
+						Title:<%=po.getTitle()%></h4>
+					<p>
+						<b>Content: <%=po.getContent()%></b>
+					</p>
+					<p>
+						<b>Uploaded By: <%=user1.getName()%></b>
+					</p>
+					<p>
+						<b>Upload Date: <%=po.getPdate()%></b>
+					</p>
+				</div>
+				<div class="">
+					<a href="#">Delete</a> <a href="#">Edit</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<%
+	}
+	}
+	%>
+</div>
+
 <%@ include file="all_components/footer.jsp"%>
-</body>
-</html>
